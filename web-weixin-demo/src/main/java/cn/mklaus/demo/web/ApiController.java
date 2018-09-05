5943e0a7a62e1de620981f744d9b3350b2b22796
klaus <klaus@klausdeMacBook-Pro.local>
2018-09-05 16:01:46 +0800

    update

--- web-weixin-demo/src/main/java/cn/mklaus/demo/web/ApiController.java
+++ web-weixin-demo/src/main/java/cn/mklaus/demo/web/ApiController.java
@@ -1,8 +1,27 @@
 package cn.mklaus.demo.web;
 
+import cn.mklaus.framework.bean.Response;
+import cn.mklaus.framework.support.Qinius;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author klaus
  * @date 2018/9/4 下午6:29
  */
+@RestController
+@RequestMapping("api")
 public class ApiController {
+
+    @GetMapping("qiniu/token")
+    public JSONObject uptoken() {
+        return Response.ok()
+                .put("action", Qinius.getAction())
+                .put("token", Qinius.getUpToken())
+                .put("bucket_url", Qinius.getBucketUrl())
+                .build();
+    }
+
 }
--- web-weixin-demo/src/main/java/cn/mklaus/demo/web/AppController.java
+++ web-weixin-demo/src/main/java/cn/mklaus/demo/web/AppController.java
@@ -1,8 +1,20 @@
 package cn.mklaus.demo.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author klaus
  * @date 2018/9/4 下午6:29
  */
+@Controller
+@RequestMapping
 public class AppController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
 }
