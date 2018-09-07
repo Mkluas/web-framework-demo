47734f786bed147e08accd043744467b9388af20
klaus <klaus@klausdeMacBook-Pro.local>
2018-09-07 12:13:31 +0800

    update

--- web-admin-demo/src/test/java/cn/mklaus/demo/web/AdminControllerTest.java
+++ web-admin-demo/src/test/java/cn/mklaus/demo/web/AdminControllerTest.java
@@ -8,12 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -40,8 +45,42 @@ public class AdminControllerTest extends AbstractShiroTest {
 
     }
 
+
+    // 加上注解回滚
+    @Transactional
+    @Rollback
+    @Test
+    public void saveAdmin() throws Exception {
+        String account = "klaus";
+        String username = "谢先生";
+        String mobile = "18027247247";
+        String email = "xie.jinye@163.com";
+        MockHttpServletRequestBuilder save = post("/admin/save")
+                .session(this.session)
+                .param("account", account)
+                .param("username", username)
+                .param("mobile", mobile)
+                .param("email", email);
+
+        mvc.perform(save)
+                .andDo(print())
+                .andExpect(jsonPath("admin.account").value(account))
+                .andExpect(jsonPath("admin.username").value(username))
+                .andExpect(jsonPath("admin.mobile").value(mobile))
+                .andExpect(jsonPath("admin.email").value(email))
+                .andExpect(jsonPath("admin.id").isNumber())
+                .andExpect(logicOk());
+    }
+
     @Test
     public void listAdmin() throws Exception {
+        mvc.perform(get("/admin/list").session(session))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void afterAdmin() throws Exception {
         mvc.perform(get("/admin/list").session(session))
                 .andDo(print())
                 .andExpect(status().isOk());
